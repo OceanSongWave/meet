552a27212b42ff73f3740d47849ccf5822dfe573
OceanSong <65198135+OceanSongWave@users.noreply.github.com>
2021-03-21 17:19:29 -0400

    BDD, end-to-end testing

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,10 +1,9 @@
 import React, { Component } from "react";
 import { ErrorAlert } from "./Alert";
-// import { ErrorAlert, WarningAlert } from './Alert';
 
 class NumberOfEvents extends Component {
   state = {
-    numberOfEvents: 20,
+    numberOfEvents: 32,
   };
 
   handleInputChanged = (event) => {
@@ -14,7 +13,7 @@ class NumberOfEvents extends Component {
 
     if (value < 1) {
       this.setState({
-        infoText: "Select number from 1 to 20",
+        infoText: "Select number from 1 to 32",
       });
     } else {
       this.setState({
@@ -38,50 +37,6 @@ class NumberOfEvents extends Component {
       </div>
     );
   }
-
-
-  // submitNumber = (event) => {
-  //   const value = event.target.value;
-  //   if (value > 32 || value < 1 || value === "") {
-  //     this.setState({
-  //       infoText: "Please enter number between 1 ~ 32",
-  //       warningText: "",
-  //       numberOfEvents: value,
-  //     });
-  //   } else if (isNaN(value)) {
-  //     this.setState({
-  //       infoText: "Please enter number",
-  //       numberOfEvents: value,
-  //       warningText: "",
-  //     });
-  //     if (value.includes("fuck") || value.includes("shit")) {
-  //       this.setState({
-  //         numberOfEvents: value,
-  //         warningText: "What??!! 🤬🤬🤬",
-  //         infoText: "",
-  //       });
-  //     }
-  //   } else {
-  //     this.props.updateEvents(null, value);
-  //     this.setState({ numberOfEvents: value, infoText: "", warningText: "" });
-  //   }
-  // };
-
-  // render() {
-  //   return (
-  //     <div className="numberOfEvents">
-  //       {/* <ErrorAlert text={this.state.infoText} /> */}
-  //       {/* <WarningAlert text={this.state.warningText} /> */}
-  //       <label>Number of Events: </label>
-  //       <input
-  //         type="text"
-  //         className="numberInput"
-  //         value={this.state.numberOfEvents}
-  //         onChange={this.submitNumber}
-  //       />
-  //     </div>
-  //   );
-  // }
 }
 
 export default NumberOfEvents;
--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -39,3 +39,23 @@ describe("show/hide an event details", () => {
     expect(eventDetails).toBeNull();
   });
 });
+
+describe('Filter events by city', () => {
+  let browser;
+  let page;
+  beforeAll(async () => {
+    jest.setTimeout(30000);
+    browser = await puppeteer.launch();
+    page = await browser.newPage();
+    await page.goto('http://localhost:3000/');
+  });
+
+  afterAll(() => {
+    browser.close();
+  });
+
+  test('When user hasn’t searched for a city, show upcoming events from all cities', async () => {
+    const events = await page.$$('.event');
+    expect(events).toHaveLength(2);
+  });
+});
