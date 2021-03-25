db52f8bdb424386a186433456daa4913cbc2f811
OceanSong <65198135+OceanSongWave@users.noreply.github.com>
2021-03-24 20:42:04 -0400

    add alerts & styling

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -19,7 +19,7 @@ class CitySearch extends Component {
     if (suggestions.length === 0) {
       return this.setState({
         query: value,
-        infoText: 'We can not find the city you are looking for. Please try another city',
+        infoText: 'We can not find the city you are looking for. Please try another city.',
       });
     } else {
       return this.setState({
