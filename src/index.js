8628e8760af48bf8a99771ebd81f6e0936338235
OceanSong <65198135+OceanSongWave@users.noreply.github.com>
2021-03-27 21:02:52 -0400

    service worker

--- src/index.js
+++ src/index.js
@@ -16,7 +16,8 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.unregister();
+// serviceWorkerRegistration.unregister();
+serviceWorkerRegistration.register();
 atatus.config('37c2462244c9403c9d1d12c4f2de257b').install();
 
 // If you want to start measuring performance in your app, pass a function
